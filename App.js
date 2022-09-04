fed5e0f1efe4f1b6a252b6e8c50de8365c41b8a3
k0j1314 <k0j1314@LAPTOP-J9RAIGSV.localdomain>
2022-09-04 16:29:40 +0900

    made header, start on first page buttons

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -6,51 +5,31 @@ function App() {
     <div className="AppUser">
       <header className="headerUser">
         <nav className="navbarUser">
-          <nav className="logopicuserhome">
-            <a href="/homepage">
-              <img className="userHome" src={logo} alt="logo" />
-            </a>
-          </nav>
           <ul className="nav-menu-home">
             <li className="nav-item-home">
-              <a href="/" className="nav-link-home">
-                Look at list
-              </a>
-            </li>
-            <li className="nav-item-home">
-              <a href="/" className="nav-link-home">
-                Add to List
-              </a>
-            </li>
-            <li className="nav-item-home">
-              <button className="button-nav-link" onClick={handleLogout}>
-                {" "}
-                Sign Out{" "}
-              </button>
+              {/*   <button className="button-nav-link"  onClick={handleLogout}  >  */}
+              <button className="button-nav-link"> Sign Out </button>
             </li>
           </ul>
         </nav>
       </header>
 
-      {/*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>REEEEEEEEEE HELP</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-
-      */}
+      {
+        // END OF NAVBAR }
+      }
+      <div className="clickTable"></div>
+      <table>
+        <tr>
+          <th>CLICK OPTIONS </th>
+        </tr>
+        <tr>
+          <td>ADD</td> <td> LOOKUP</td>
+        </tr>
+        <tr>
+          <td>REMOVE</td>
+          <td>SOMETHING</td>
+        </tr>
+      </table>
     </div>
   );
 }
